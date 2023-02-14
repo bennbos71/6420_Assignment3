2bddc5300c6edc3474532009914c4576e9e4a870
bennbos71 <johnbos@mail.weber.edu>
2023-02-13 20:27:37 -0700

    Created beginning project files

---
+++ knuth_morris_pratt.cpp
@@ -0,0 +1,42 @@
+//                 Ben Bos 
+//         CS 6420 
+//         Assignment #3
+//    Dr. Rague 
+//    Due: 02/19/23 
+//    Version: 1.0 
+// ----------------------------------------------------------------- 
+// This program finds a prefix within a given text using
+// the Knuth-Morris-Pratt Algorithm
+// ----------------------------------------------------------------- 
+ 
+// Compiler directives 
+#include <iostream>
+
+#include "alg_strings.h"
+ 
+using namespace std; 
+
+int main() {
+    cout << "Hello World from Knuth-Morris-Pratt!" << endl;
+    return 0;
+}
+
+
+
+// ----------------------------------------------------------------- 
+// This class represents a circle that can be displayed on 
+// a console window. 
+// 
+// Version 1.1 
+// ---------------------------------------------------------- 
+
+
+
+
+// Variable declarations 
+//    int Num_Cases;            // The number of cases of data values to process 
+//    int Installations;        // The number of installations 
+//    double Yards_Of_Cable,    // The yards of cable installed 
+//     Feet_Of_Cable, // The number of yards of cable converted to 
+// feet 
+//     Revenue;       // The revenue generated for this case of data 
