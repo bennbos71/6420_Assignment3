2eda6cab11b67d7efeee9547e3d2e96571d5ea74
bennbos71 <johnbos@mail.weber.edu>
2023-02-15 20:49:19 -0700

    Added Non-working compute prefix

--- knuth_morris_pratt.cpp
+++ knuth_morris_pratt.cpp
@@ -11,24 +11,48 @@
  
 // Compiler directives 
 #include <iostream>
-
 #include "alg_strings.h"
  
 using namespace std; 
 
+// ----------------------------------------------------------------- 
+// This function computes the prefix function pi and returns it as
+// a string
+// 
+// Version 1.0
+// -----------------------------------------------------------------
+string* compute_prefix(string p){
+    int m = p.length();
+    string* pi = new string[m];
+    pi[0] = to_string(0);
+    int k = 0;
+    for (int q = 2; q < m; q++) {
+        while (k > 0 && p[k + 1] != p[q]) {
+            k = stoi(pi[k]);
+            if (p[k + 1] == p[q]) {
+                k = k + 1;
+            }
+        }
+        // cout << k << endl;
+        pi[q] = k;
+    }
+    return pi;
+}
+
 int main() {
-    cout << "Hello World from Knuth-Morris-Pratt!" << endl;
+    string input = "01111000110101011010";
+    string* output = new string[input.length()];
+    output = compute_prefix(input);
+    for (int i = 0; i < input.length(); i++) {
+        cout << output[i] << endl;
+    }
+    // cout << compute_prefix(input) << endl;
     return 0;
 }
 
 
 
-// ----------------------------------------------------------------- 
-// This class represents a circle that can be displayed on 
-// a console window. 
-// 
-// Version 1.1 
-// ---------------------------------------------------------- 
+
 
 
 
